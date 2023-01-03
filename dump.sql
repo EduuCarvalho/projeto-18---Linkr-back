85dcefb5acbe8f2d20e840cdee088276e867ba78
Cintra <gui7lk@gmail.com>
2023-01-03 19:56:58 -0300

    chorus: adjust for connection on render

--- dump.sql
+++ dump.sql
@@ -1,85 +1,569 @@
-CREATE TABLE users (
-	"id" serial NOT NULL,
-	"name" TEXT NOT NULL,
-	"password" TEXT NOT NULL,
-	"email" TEXT NOT NULL UNIQUE,
-	"picture_url" TEXT NOT NULL UNIQUE,
-	"created_at" TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-	CONSTRAINT "users_pk" PRIMARY KEY ("id")
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.12 (Ubuntu 12.12-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.12 (Ubuntu 12.12-0ubuntu0.20.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: hashtags; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.hashtags (
+    id integer NOT NULL,
+    name integer NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
+--
+-- Name: hashtags_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
 
-CREATE TABLE sessions (
-	"id" serial NOT NULL,
-	"token" TEXT NOT NULL UNIQUE,
-	"user_id" integer NOT NULL,
-	"created_at" TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-	CONSTRAINT "sessions_pk" PRIMARY KEY ("id")
+CREATE SEQUENCE public.hashtags_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: hashtags_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.hashtags_id_seq OWNED BY public.hashtags.id;
+
+
+--
+-- Name: hashtags_name_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.hashtags_name_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: hashtags_name_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.hashtags_name_seq OWNED BY public.hashtags.name;
+
+
+--
+-- Name: likes; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.likes (
+    id integer NOT NULL,
+    post_id integer NOT NULL,
+    user_id integer NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
+--
+-- Name: likes_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
 
-CREATE TABLE posts (
-	"id" serial NOT NULL,
-	"user_id" integer NOT NULL,
-	"link_id" integer NOT NULL,
-	"description" TEXT NOT NULL,
-	"created_at" TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-	CONSTRAINT "posts_pk" PRIMARY KEY ("id")
+CREATE SEQUENCE public.likes_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: likes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.likes_id_seq OWNED BY public.likes.id;
+
+
+--
+-- Name: links; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.links (
+    id integer NOT NULL,
+    url text NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
+--
+-- Name: links_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
 
-CREATE TABLE links (
-	"id" serial NOT NULL,
-	"user_id" integer NOT NULL,
-	"url" TEXT NOT NULL,
-	"created_at" TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-	CONSTRAINT "links_pk" PRIMARY KEY ("id")
+CREATE SEQUENCE public.links_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: links_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.links_id_seq OWNED BY public.links.id;
+
+
+--
+-- Name: post_hashtags; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.post_hashtags (
+    id integer NOT NULL,
+    hastag_id integer NOT NULL,
+    post_id integer NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
+--
+-- Name: post_hashtags_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
 
-CREATE TABLE hastags (
-	"id" serial NOT NULL,
-	"name" serial NOT NULL,
-	"created_at" TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-	CONSTRAINT "hastags_pk" PRIMARY KEY ("id")
+CREATE SEQUENCE public.post_hashtags_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: post_hashtags_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.post_hashtags_id_seq OWNED BY public.post_hashtags.id;
+
+
+--
+-- Name: posts; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.posts (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    link_id integer NOT NULL,
+    description text NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
+--
+-- Name: posts_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
 
-CREATE TABLE post_hastags (
-	"id" serial NOT NULL,
-	"hastag_id" integer NOT NULL,
-	"post_id" integer NOT NULL,
-	"created_at" TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-	CONSTRAINT "post_hastags_pk" PRIMARY KEY ("id")
+CREATE SEQUENCE public.posts_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.posts_id_seq OWNED BY public.posts.id;
+
+
+--
+-- Name: sessions; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.sessions (
+    id integer NOT NULL,
+    token text NOT NULL,
+    user_id integer NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
+--
+-- Name: sessions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
 
-CREATE TABLE likes (
-	"id" serial NOT NULL,
-	"post_id" integer NOT NULL,
-	"user_id" integer NOT NULL,
-	"created_at" TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-	CONSTRAINT "likes_pk" PRIMARY KEY ("id")
+CREATE SEQUENCE public.sessions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.sessions_id_seq OWNED BY public.sessions.id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name text NOT NULL,
+    password text NOT NULL,
+    email text NOT NULL,
+    picture_url text NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 
-ALTER TABLE "sessions" ADD CONSTRAINT "sessions_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
 
-ALTER TABLE "posts" ADD CONSTRAINT "posts_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
-ALTER TABLE "posts" ADD CONSTRAINT "posts_fk1" FOREIGN KEY ("link_id") REFERENCES "links"("id");
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
-ALTER TABLE "links" ADD CONSTRAINT "links_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
 
-ALTER TABLE "post_hastags" ADD CONSTRAINT "post_hastags_fk0" FOREIGN KEY ("hastag_id") REFERENCES "hastags"("id");
-ALTER TABLE "post_hastags" ADD CONSTRAINT "post_hastags_fk1" FOREIGN KEY ("post_id") REFERENCES "posts"("id");
+--
+-- Name: hashtags id; Type: DEFAULT; Schema: public; Owner: -
+--
 
-ALTER TABLE "likes" ADD CONSTRAINT "likes_fk0" FOREIGN KEY ("post_id") REFERENCES "posts"("id");
-ALTER TABLE "likes" ADD CONSTRAINT "likes_fk1" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+ALTER TABLE ONLY public.hashtags ALTER COLUMN id SET DEFAULT nextval('public.hashtags_id_seq'::regclass);
+
+
+--
+-- Name: hashtags name; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.hashtags ALTER COLUMN name SET DEFAULT nextval('public.hashtags_name_seq'::regclass);
+
+
+--
+-- Name: likes id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.likes ALTER COLUMN id SET DEFAULT nextval('public.likes_id_seq'::regclass);
+
+
+--
+-- Name: links id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.links ALTER COLUMN id SET DEFAULT nextval('public.links_id_seq'::regclass);
+
+
+--
+-- Name: post_hashtags id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.post_hashtags ALTER COLUMN id SET DEFAULT nextval('public.post_hashtags_id_seq'::regclass);
+
+
+--
+-- Name: posts id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_id_seq'::regclass);
+
+
+--
+-- Name: sessions id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions ALTER COLUMN id SET DEFAULT nextval('public.sessions_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: hashtags; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: likes; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: links; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: post_hashtags; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: posts; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Name: hashtags_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.hashtags_id_seq', 1, false);
+
+
+--
+-- Name: hashtags_name_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.hashtags_name_seq', 1, false);
+
+
+--
+-- Name: likes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.likes_id_seq', 1, false);
+
+
+--
+-- Name: links_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.links_id_seq', 1, false);
+
+
+--
+-- Name: post_hashtags_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.post_hashtags_id_seq', 1, false);
+
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.posts_id_seq', 1, false);
+
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.sessions_id_seq', 1, false);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+
+
+--
+-- Name: hashtags hashtags_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.hashtags
+    ADD CONSTRAINT hashtags_pk PRIMARY KEY (id);
+
+
+--
+-- Name: likes likes_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT likes_pk PRIMARY KEY (id);
+
+
+--
+-- Name: links links_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.links
+    ADD CONSTRAINT links_pk PRIMARY KEY (id);
+
+
+--
+-- Name: links links_url_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.links
+    ADD CONSTRAINT links_url_key UNIQUE (url);
+
+
+--
+-- Name: post_hashtags post_hashtags_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.post_hashtags
+    ADD CONSTRAINT post_hashtags_pk PRIMARY KEY (id);
+
+
+--
+-- Name: posts posts_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_pk PRIMARY KEY (id);
+
+
+--
+-- Name: sessions sessions_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT sessions_pk PRIMARY KEY (id);
+
+
+--
+-- Name: sessions sessions_token_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT sessions_token_key UNIQUE (token);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_name_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_name_key UNIQUE (name);
+
+
+--
+-- Name: users users_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pk PRIMARY KEY (id);
+
+
+--
+-- Name: likes likes_fk0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT likes_fk0 FOREIGN KEY (post_id) REFERENCES public.posts(id);
+
+
+--
+-- Name: likes likes_fk1; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT likes_fk1 FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: post_hashtags post_hashtags_fk0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.post_hashtags
+    ADD CONSTRAINT post_hashtags_fk0 FOREIGN KEY (hastag_id) REFERENCES public.hashtags(id);
+
+
+--
+-- Name: post_hashtags post_hashtags_fk1; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.post_hashtags
+    ADD CONSTRAINT post_hashtags_fk1 FOREIGN KEY (post_id) REFERENCES public.posts(id);
+
+
+--
+-- Name: posts posts_fk0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_fk0 FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: posts posts_fk1; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_fk1 FOREIGN KEY (link_id) REFERENCES public.links(id);
+
+
+--
+-- Name: sessions sessions_fk0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT sessions_fk0 FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
