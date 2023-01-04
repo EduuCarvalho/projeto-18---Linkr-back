ea6fc4f2e26ea8df1009f2f117f0b577d90ae1b0
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2023-01-04 07:33:43 -0300

    fix: send only neccessary info from the users searched

--- src/repositories/usersRepository.js
+++ src/repositories/usersRepository.js
@@ -1,7 +1,8 @@
 import connectionDB from "../database/database.js";
 
 export function searchUsers(pattern) {
-  return connectionDB.query("SELECT * FROM users WHERE name LIKE $1", [
-    pattern + "%",
-  ]);
+  return connectionDB.query(
+    "SELECT id, name, picture_url FROM users WHERE name LIKE $1",
+    [pattern + "%"]
+  );
 }
