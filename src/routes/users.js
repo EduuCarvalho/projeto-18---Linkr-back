d33b728484e670d3518e8c9858d4579fa5a4877f
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2023-01-07 17:45:30 -0300

    feat: getUserPosts created

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getUsersBySearch } from "../controllers/usersController.js";
+import { getUserPosts, getUsersBySearch } from "../controllers/usersController.js";
+import authValidation from "../middlewares/authValidation.js";
 
 const users = Router();
 
-users.get('/users', getUsersBySearch);
+users.get('/users', authValidation, getUsersBySearch);
+users.get('/users/:id', authValidation, getUserPosts);
 
 export default users;
