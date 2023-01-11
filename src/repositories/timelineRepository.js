b3612b010611ba8380aa594365e5ac9ac641b5db
lcfernandez <lcfernandez@cos.ufrj.br>
2023-01-11 17:38:27 -0300

    feat: include total of comments of each post

--- src/repositories/timelineRepository.js
+++ src/repositories/timelineRepository.js
@@ -20,7 +20,8 @@ export async function getPosts() {
         SELECT p.id, p.description,
             u.name, u.picture_url, u.id as "ownerId",
             l.url,
-            COALESCE(s.user_id, NULL) as "who_shared_id", COUNT(s.post_id) as shares
+            COALESCE(s.user_id, NULL) as "who_shared_id", COUNT(s.post_id) as shares,
+            COALESCE(COUNT(c.id), 0) as "total_comments"
         FROM posts as p
             JOIN users as u
                 ON p.user_id = u.id
@@ -28,6 +29,8 @@ export async function getPosts() {
                 ON p.link_id = l.id
             LEFT JOIN shares as s
                 ON s.post_id = p.id
+            LEFT JOIN comments c
+                ON c.post_id = p.id
         WHERE u.id = s.user_id OR s.user_id IS NULL
         GROUP BY p.id, u.id, s.user_id, l.url
         ORDER BY id DESC
