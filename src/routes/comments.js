063fe3be399feb3e8c56570a82d6b5154cda19c4
lcfernandez <lcfernandez@cos.ufrj.br>
2023-01-11 00:36:57 -0300

    feat: create comments model and validation

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,10 +1,11 @@
 import { create } from "../controllers/commentsControllers.js";
+import { commentsPostValidation } from "../middlewares/commentsValidation.js";
 import authValidation from "../middlewares/authValidation.js";
 
 import { Router } from "express";
 
 const router = Router();
 
-router.post("/comments", authValidation, create);
+router.post("/comments", authValidation, commentsPostValidation, create);
 
 export default router;
